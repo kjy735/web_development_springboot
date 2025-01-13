39a8ef2ab968b96de6106c32342ad7443a286d37
kjy735 <wodus3806@gmail.com>
2025-01-13 21:44:58 +0900

    feat: 20250113 chapter07 CRUD and View Ended

--- kimjayeon_springboot/chapter07/src/main/java/me/kimjaeyeon/springbootdeveloper/controller/BlogViewController.java
+++ kimjayeon_springboot/chapter07/src/main/java/me/kimjaeyeon/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,16 @@
 package me.kimjaeyeon.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.kimjaeyeon.springbootdeveloper.domain.Article;
 import me.kimjaeyeon.springbootdeveloper.dto.ArticleListViewResponse;
+import me.kimjaeyeon.springbootdeveloper.dto.ArticleViewResponse;
 import me.kimjaeyeon.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,8 +27,27 @@ public class BlogViewController {
 
         model.addAttribute("articles", articles);   // "articles"키에 articles list를 담았습니다.
 
-        return "articleList";   // -> 우리가 이거 다음에 만들어야될 파일의 위치 및 파이명
+        return "articleList";   // -> 우리가 이거 다음에 만들어야될 파일의 위치 및 파일명
     }
 
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
+    @GetMapping("/new-article")
+    // 1. id 키를 가진 쿼리 파라미터의 값을 id 변수에 매핑(id는 없을 수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if(id == null) {   // 2. id가 없으면 새로 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        }else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
+    }
 
 }
