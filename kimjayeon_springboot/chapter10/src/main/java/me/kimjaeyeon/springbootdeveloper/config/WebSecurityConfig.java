9820d20cb823117e081cced37ac4b520d2fc8368
kjy735 <wodus3806@gmail.com>
2025-01-20 21:42:54 +0900

    feat: OAuthLogin

--- kimjayeon_springboot/chapter10/src/main/java/me/kimjaeyeon/springbootdeveloper/config/WebSecurityConfig.java
+++ kimjayeon_springboot/chapter10/src/main/java/me/kimjaeyeon/springbootdeveloper/config/WebSecurityConfig.java
@@ -1,72 +1,72 @@
-package me.kimjaeyeon.springbootdeveloper.config;
-
-import lombok.RequiredArgsConstructor;
-import me.kimjaeyeon.springbootdeveloper.service.UserDetailService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class WebSecurityConfig {
-    private final UserDetailService userDetailService;
-
-    @Bean
-    public WebSecurityCustomizer configure() {
-        return (web) -> web.ignoring()
-                .requestMatchers(toH2Console())
-                .requestMatchers(new AntPathRequestMatcher("/static/**"));
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        return http
-                .authorizeRequests(auth ->auth  // 3. 인증 / 인가 설정
-                        .requestMatchers(
-                                new AntPathRequestMatcher("/login"),
-                                new AntPathRequestMatcher("/signup"),
-                                new AntPathRequestMatcher("/user")
-                        ).permitAll()
-                        .anyRequest().authenticated())
-                .formLogin(formLogin -> formLogin   // 4. 폼 기반 로그인 설정
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/articles")
-                )
-                .logout(logout -> logout        // 로그아웃 설정
-                        .logoutSuccessUrl("/login")
-                        .invalidateHttpSession(true)
-                )
-                .csrf(AbstractHttpConfigurer::disable)              // 6.
-                .build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http,
-                                                       BCryptPasswordEncoder bCryptPasswordEncoder,
-                                                       UserDetailService userDetailService) throws Exception {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailService);  // 8. 사용자 정보 서비스 설정
-        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
-        return new ProviderManager(authProvider);
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//package me.kimjaeyeon.springbootdeveloper.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import me.kimjaeyeon.springbootdeveloper.service.UserDetailService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.ProviderManager;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//
+//import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class WebSecurityConfig {
+//    private final UserDetailService userDetailService;
+//
+//    @Bean
+//    public WebSecurityCustomizer configure() {
+//        return (web) -> web.ignoring()
+//                .requestMatchers(toH2Console())
+//                .requestMatchers(new AntPathRequestMatcher("/static/**"));
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        return http
+//                .authorizeRequests(auth ->auth  // 3. 인증 / 인가 설정
+//                        .requestMatchers(
+//                                new AntPathRequestMatcher("/login"),
+//                                new AntPathRequestMatcher("/signup"),
+//                                new AntPathRequestMatcher("/user")
+//                        ).permitAll()
+//                        .anyRequest().authenticated())
+//                .formLogin(formLogin -> formLogin   // 4. 폼 기반 로그인 설정
+//                        .loginPage("/login")
+//                        .defaultSuccessUrl("/articles")
+//                )
+//                .logout(logout -> logout        // 로그아웃 설정
+//                        .logoutSuccessUrl("/login")
+//                        .invalidateHttpSession(true)
+//                )
+//                .csrf(AbstractHttpConfigurer::disable)              // 6.
+//                .build();
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(HttpSecurity http,
+//                                                       BCryptPasswordEncoder bCryptPasswordEncoder,
+//                                                       UserDetailService userDetailService) throws Exception {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userDetailService);  // 8. 사용자 정보 서비스 설정
+//        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+//        return new ProviderManager(authProvider);
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 
 }
 
--- kimjayeon_springboot/chapter10/src/main/java/me/kimjaeyeon/springbootdeveloper/util/CookieUtil.java
+++ kimjayeon_springboot/chapter10/src/main/java/me/kimjaeyeon/springbootdeveloper/util/CookieUtil.java
@@ -3,6 +3,9 @@ package me.kimjaeyeon.springbootdeveloper.util;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.SerializationUtils;
+
+import java.util.Base64;
 
 public class CookieUtil {
     // 요청값(이름, 값, 만료기간)을 바탕으로 쿠키 추가
@@ -43,5 +46,26 @@ public class CookieUtil {
             20240120에 객체를 직렬화 / 역직렬화하는 메서드를 구현할 예정
  */
 
+// 객체를 직렬화 해서 쿠키의 값으로 변환
+    public static String serialize(Object obj) {
+        return Base64.getUrlEncoder()
+                .encodeToString(SerializationUtils.serialize(obj));
+    }
 
+    // 쿠키를 역직렬화해서 객체로 변환
+    public static <T> T deserialize(Cookie cookie, Class<T> cls) {
+        return cls.cast(
+                SerializationUtils.deserialize(
+                Base64.getDecoder().decode(cookie.getValue())
+        )
+        );
+    }
 }
+/*
+    사용자 정보를 조회해 users테이블에 사용자 정보가 있다면 리소스 서버(OAuth2 관련)에서
+    제공해주는 이름을 업데이트 했습니다
+
+    사용자 정보가 없다면 새 사용자를 생성하고 DB에 저장할 수 있도록 하겠습니다
+
+    먼저, domain에 User.java를 수정하겠습니다
+ */
